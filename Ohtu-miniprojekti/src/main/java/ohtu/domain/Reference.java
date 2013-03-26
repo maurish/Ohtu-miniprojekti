1880f2712e9f7cb785fb2f438d0ee637dbe5ee56
Anniina <anniina.sallinen@cs.helsinki.fi>
2013-03-26 17:29:33 +0200

    Added addRef.jsp and some methods for ReferenceService, also overrode them in referenceServiceImpl. Added author and name for class Reference

--- Ohtu-miniprojekti/src/main/java/ohtu/domain/Reference.java
+++ Ohtu-miniprojekti/src/main/java/ohtu/domain/Reference.java
@@ -1,4 +1,33 @@
 package ohtu.domain;
 
 public class Reference {
+    
+    private String author;
+    private String refName;
+
+    public Reference(String author, String name) {
+        this.author = author;
+        this.refName = name;
+    }
+
+    public void setName(String name) {
+        this.refName = name;
+    }
+
+    public String getName() {
+        return refName;
+    }
+
+    @Override
+    public String toString() {
+        return "Reference{" + "author=" + author + '}';
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
