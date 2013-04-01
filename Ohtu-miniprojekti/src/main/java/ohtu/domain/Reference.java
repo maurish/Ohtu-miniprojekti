f794d170406d988b61b47548687199a15aae100f
maurish <pauli.perala@helsinki.fi>
2013-04-01 18:21:18 +0300

    configured h2 database

--- Ohtu-miniprojekti/src/main/java/ohtu/domain/Reference.java
+++ Ohtu-miniprojekti/src/main/java/ohtu/domain/Reference.java
@@ -1,9 +1,30 @@
 package ohtu.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Reference {
 
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
     private String author;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Reference(String author, String name) {
         this.author = author;
--- Ohtu-miniprojekti/src/main/java/ohtu/service/ReferenceServiceImpl.java
+++ Ohtu-miniprojekti/src/main/java/ohtu/service/ReferenceServiceImpl.java
@@ -1,25 +1,24 @@
 package ohtu.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import ohtu.domain.Reference;
+import ohtu.repository.ReferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReferenceServiceImpl implements ReferenceService {         //Käytetään tietokannan käsittelyyn
-    
-    List<Reference> refs;
 
-    public ReferenceServiceImpl() {
-        this.refs=new ArrayList<Reference>();
-    }
+    @Autowired
+    ReferenceRepository repo;
+
     @Override
     public void add(Reference ref) {
-        refs.add(ref);
+        repo.save(ref);
     }
 
     @Override
     public List<Reference> listAll() {
-        return refs;
+        return repo.findAll();
     }
 }
