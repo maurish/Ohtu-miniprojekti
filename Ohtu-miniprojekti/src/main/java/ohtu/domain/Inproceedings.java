2b97db162ff37f112c7f72f31ea61dc1b5747d2c
svanteha <svanteha@dcb-16.cs.helsinki.fi>
2013-04-05 14:31:04 +0300

    added classes article and inproc

---
+++ Ohtu-miniprojekti/src/main/java/ohtu/domain/Inproceedings.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author svanteha
+ */
+@Entity
+@Table
+public class Inproceedings extends Reference {
+    
+}
